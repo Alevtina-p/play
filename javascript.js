ff66d5c08f45a426cb8d57b8580151e2a10ac5f8
Alevtina-p <alevtina.pet2017@yandex.ru>
2022-03-15 16:03:39 +0300

    creates five rounds and total

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
 const r='rock';
 const p='paper';
 const s='scissors';
-function computerPlay() {
+const draw='draw';
+let comp=0;
+let player=0;
+
+
+  function computerPlay() {
 
     let number=Math.floor(Math.random()*3+1);
     switch (number) {
@@ -10,21 +15,38 @@ function computerPlay() {
       case 2: return p;
       break;
       default: return s;}
-}
-//console.log(computerPlay());
-const playerSelection=(prompt('input value: Rock, Paper, Scissors','')).toLowerCase();
-const computerSelection=computerPlay();
-function playRound(playerSelection,computerSelection){
-   const comp='you lost';
-   const player='comp lost'; 
-   const draw='draw';
-     if (playerSelection===computerSelection) { return draw}
-     else if (playerSelection===r){
-          if (computerSelection===p) {return comp}
-           else return player;}        
-     else if (playerSelection===p){
-          if (computerSelection===r) {return player}
-          else comp } 
-          else if (computerSelection===r) {return comp}
-            else return player  ;
-}
+  }
+    
+  function playRound(playerSelection,computerSelection) {
+     if (playerSelection===computerSelection) {return player+':'+comp}
+      else if (playerSelection===r){
+          if (computerSelection===p)  ++comp
+                             else  ++player}        
+      else if (playerSelection===p){
+          if (computerSelection===r)  ++player
+          else 
+             comp++ } 
+      else if (computerSelection===r) comp++ 
+            else { player++};
+      return player+':'+comp;      
+  }
+  function count(x,y) {
+          if (x===y) return draw
+            else if (x>y) {console.log('You won!')}
+              else console.log('Comp won!')
+  } 
+    
+ 
+  function game() {
+   for ( let i=0; i<5; i++) {
+      const playerSelection=(prompt('input value: Rock, Paper, Scissors','')).toLowerCase();
+      const computerSelection=computerPlay();
+      let score=playRound(playerSelection,computerSelection);
+      console.log('you='+playerSelection,'comp='+computerSelection,'score:'+ score);
+      if (i===2) {
+       count(score.charAt(0),score.charAt(2)) 
+     }
+   }
+  }
+  
+   game();
